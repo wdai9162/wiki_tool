7d25b76f1b3cca5d598e8569c05e323394f73313
wdai9162 <wdai9162@uni.sydney.edu.au>
2020-04-23 00:10:36 +1000

    overall stats API update, first three are available now

--- group-back/app/controllers/overall.controller.js
+++ group-back/app/controllers/overall.controller.js
@@ -17,47 +17,48 @@ const path = require('path');
 //import bot user filter file
 var botUser = fs.readFileSync(path.join(__dirname,  '/../user_filter/bots.txt')).toString().split("\n");
 
-module.exports.sendOverallStats = function (req, res) {
+module.exports.byRevNumbers = function (req, res) {
     //extract user selection of the number of how many highest/lowest results to view
     //const queryNumber = req.body.userSelection;
-    const queryNumber = 2;
-
+    const queryNumber = 3;
+    
     //Return sorted count of total revisions of each article, top 2 and bottom 2 are needed
-    const aggCountOps1 = [
+    const queryByRevNumber = [
         {
             $group : {
                  _id : "$title",
                  count: { $sum: 1 }
             }
         },
-        { $sort: { count: -1 } },
-        {
-          $limit: 2
-        }
-    ]
-
-    //Return sorted count of groups of registered users (non bots) for each article.
-    //BOT NOT FILTERED YET!!!!
-    const aggCountOps2 = [
-        {
-            $group : {
-                _id :  {
-                    title: "$title",
-                    user: "$user"
-                }
-            }
-        },
-        {
-            $group : {
-                _id : "$_id.title",
-                count: { $sum: 1 }
-            }
-        },
         { $sort: { count: -1 } }
     ]
 
-    //The top two articles edited by the largest group of registered users (non bots) and their group size. Each wiki article is edited by a number of users, some making multiple revisions. The number of unique users is a good indicator of an article’s popularity.
-    const aggCountOps3 = [
+    Revinfo.aggregate(queryByRevNumber)
+    .then(result => {
+        res.status(200).json({
+            confirmation: 'success',
+            number_queried: queryNumber,
+            highest: result.slice(0,queryNumber),
+            lowest: result.slice(queryNumber*-1),
+        })
+    })
+    .catch(err => {
+        res.json({
+            confirmation:'failed',
+            message: err
+        })
+    });
+}
+
+module.exports.byRegUsers  = function (req, res) {
+    //extract user selection of the number of how many highest/lowest results to view
+    //const queryNumber = req.body.userSelection;
+    const queryNumber = 3;
+    
+    //The top two articles edited by the largest group of registered users (non bots) and their group size. 
+    //Each wiki article is edited by a number of users, some making multiple revisions. 
+    //The number of unique users is a good indicator of an article’s popularity.
+    const queryByRegUsers = [
         {
             "$group" : {
                 "_id" : {
@@ -106,28 +107,50 @@ module.exports.sendOverallStats = function (req, res) {
         },
         { "$sort": { "users": -1 } }
     ]
-/*
-    var data = [123];
 
-    Revinfo.aggregate(aggCountOps1, (err, result) => {
-        if (err) {
-            res.json({
-                confirmation:'failed',
-                message: err
-            })
-        };
-        data.push(result[0,3]);
-    })
-
-    res.status(200).send(data);
-*/
-
-
-    Revinfo.aggregate(aggCountOps1)
-    .then(revs => {
+    Revinfo.aggregate(queryByRegUsers)
+    .then(result => {
         res.status(200).json({
-            message: 'success',
-            data:revs
+            confirmation: 'success',
+            number_queried: queryNumber,
+            largest: result.slice(0,queryNumber),
+            least: result.slice(queryNumber*-1),
+        })
+    })
+    .catch(err => {
+        res.json({
+            confirmation:'failed',
+            message: err
+        })
+    });
+}
+
+module.exports.byArticleHistory = function (req, res) {
+    //extract user selection of the number of how many highest/lowest results to view
+    //const queryNumber = req.body.userSelection;
+    const queryNumber = 3;
+    var currentTime = new Date()
+    var currentUtcTime = currentTime.toUTCString();
+
+    //Return articles and its first revision data/creation date, in decending order
+    const queryByHistory = [
+        {
+            $group: {
+                _id: "$title",
+                firstRev: { $min: "$timestamp" }
+            }
+        }, 
+        { $sort: { firstRev: -1 } }
+    ]
+
+
+    Revinfo.aggregate(queryByHistory)
+    .then(result => {
+        res.status(200).json({
+            confirmation: 'success',
+            number_queried: queryNumber,
+            shortest: resultConsolidationShort(result,queryNumber),
+            longest: resultConsolidationLong(result,queryNumber),
         })
     })
     .catch(err => {
@@ -137,7 +160,55 @@ module.exports.sendOverallStats = function (req, res) {
         })
     });
 
+    function calculateAge(date1,date2) {
+        var now = Date.parse(date2);
+        var start = Date.parse(date1);
+        var diff = Math.abs(now - start); 
+        var ageInDays = diff/60/60/24/1000;
+        return ageInDays.toFixed(1);
+    }
+
+    function resultConsolidationShort(data,queryNumber) {
+        var result = [];
+        for (i=0;i<queryNumber;i++){
+            result.push({title: data[i]._id, age_in_days: Number(calculateAge(data[i].firstRev, currentUtcTime))}) 
+        }
+        return result; 
+    }
+    
+    function resultConsolidationLong(data,queryNumber) {
+        var result = [];
+        for (i=data.length-1;i>data.length-queryNumber-1;i--){
+            result.push({title: data[i]._id, age_in_days: Number(calculateAge(data[i].firstRev, currentUtcTime))}) 
+        }
+        return result; 
+    }
 }
+
+
+    //Return sorted count of groups of registered users (non bots) for each article.
+    //BOT NOT FILTERED YET!!!!
+    const aggCountOps2 = [
+        {
+            $group : {
+                _id :  {
+                    title: "$title",
+                    user: "$user"
+                }
+            }
+        },
+        {
+            $group : {
+                _id : "$_id.title",
+                count: { $sum: 1 }
+            }
+        },
+        { $sort: { count: -1 } }
+    ]
+
+    
+
+
 
 module.exports.sendBarChartData = function (req, res) {
     res.status(200).json({
