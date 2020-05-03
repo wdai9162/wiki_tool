41014ba1a3911858e1ed5e4978ee8a213aa6c168
sliu3017 <sliu3017@uni.sydney.edu.au>
2020-05-03 18:24:10 +1000

    some static content provide in index page

--- web-angular/src/app/view/land-author/land-author.component.ts
+++ web-angular/src/app/view/land-author/land-author.component.ts
@@ -15,15 +15,15 @@ export class LandAuthorComponent implements OnInit {
   constructor(
     private ls: LocalStorage
   ) {}
-  chartOptions = {
-    responsive: true
-  };
-  chartData = [
-    { data: [330, 600, 260, 700], label: 'Account A' },
-    { data: [120, 455, 100, 340], label: 'Account B' },
-    { data: [45, 67, 800, 500], label: 'Account C' }
-  ];
-  chartLabels = ['January', 'February', 'Mars', 'April'];
+  // chartOptions = {
+  //   responsive: true
+  // };
+  // chartData = [
+  //   { data: [330, 600, 260, 700], label: 'Account A' },
+  //   { data: [120, 455, 100, 340], label: 'Account B' },
+  //   { data: [45, 67, 800, 500], label: 'Account C' }
+  // ];
+  // chartLabels = ['January', 'February', 'Mars', 'April'];
 
   // tslint:disable-next-line:triple-equals
   isLogin = this.ls.getObject('isLogin');
