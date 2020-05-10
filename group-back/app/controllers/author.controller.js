60a3e22fa5555fa17a7120a6667762ddd1e9cd99
sliu3017 <sliu3017@uni.sydney.edu.au>
2020-05-10 12:42:21 +1000

    finished all

--- group-back/app/controllers/author.controller.js
+++ group-back/app/controllers/author.controller.js
@@ -57,12 +57,34 @@ module.exports.returnAuthorArticleAndNum = function (req, res) {
                     err: "We cannot find an article with that author name"
                 })
             }
-            return res.status(200).json({
-                confirmation: "success",
-                result: result,
-                number: result.length
-            })
+            var titleAndNumArray = [];
+
+            //result.forEach(title => {
+            for(let i = 0; i < result.length; i++){
+                let title = result[i];
+                Revinfo.find({'title':title, 'user':authorName}).then(finalResult =>{
+                    if (finalResult===null) {
+                        return res.status(401).json({
+                            confirmation: "Failed",
+                            err: "We cannot find an author with that keyword"
+                        })
+                    }
+                    let titleAndNum = {
+                        title: title,
+                        ChangeTimes: finalResult.length
+                    };
+
+                    titleAndNumArray.push(titleAndNum);
+                    if (i === result.length - 1){
+                        return res.status(200).json({
+                            confirmation: "success",
+                            result: titleAndNumArray
+                        });
+                    }
+                })
+            }
         })
+
 
 
 };
@@ -71,9 +93,9 @@ module.exports.returnArticleTimestamps = function (req, res) {
     const title = req.body.title;
     const authorName = req.body.name;
 
-    Revinfo.find({ 'title': title, 'user': authorName},{_id:0, timestamp:1})
+    Revinfo.find({'title': title, 'user': authorName}, {_id: 0, timestamp: 1})
         .then(result => {
-            if (result===null) {
+            if (result === null) {
                 return res.status(401).json({
                     confirmation: "Failed",
                     err: "We cannot find an article with that title"
@@ -83,7 +105,38 @@ module.exports.returnArticleTimestamps = function (req, res) {
                 confirmation: "success",
                 result: result
             })
+        });
+
+
+
+
+
+};
+
+
+
+module.exports.returnTitleAndNumber = function (req, res) {
+    const name = req.body.name
+    const getTitleAndNumber= [{$match: {
+            user: name
+        }}, {$group: {
+            _id: '$title',
+            sum: {
+                $sum:1
+            }
+        }}];
+
+    Revinfo.aggregate(getTitleAndNumber)
+        .then(result => {
+            res.status(200).json({
+                confirmation: 'success',
+                data: result,
+            })
         })
-
-
+        .catch(err => {
+            res.json({
+                confirmation:'failed',
+                message: err
+            })
+        })
 };
